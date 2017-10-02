74d33919cdcdf64c9b45ece4c1ff585917f9e8f1
Usuario <marquito.quito@hotmail.com>
2017-10-02 10:22:13 -0500

    cambios aplicativo 10:21

--- app/src/main/java/com/example/marvinloco/aplicativologistico/Secundaria.java
+++ app/src/main/java/com/example/marvinloco/aplicativologistico/Secundaria.java
@@ -2,31 +2,18 @@ package com.example.marvinloco.aplicativologistico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-        import android.app.Activity;
-        import android.graphics.Color;
-        import android.os.Bundle;
-
-        import android.view.Menu;
-        import android.view.MenuItem;
-        import android.view.View;
-
-        import android.webkit.WebView;
-        import android.webkit.WebViewClient;
-
-        import android.widget.Button;
-        import android.widget.EditText;
-        import android.widget.TextView;
-        import android.widget.Toast;
-
-        import java.io.FileInputStream;
-        import java.io.FileOutputStream;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Secundaria extends AppCompatActivity {
     Button btn_2, btn_3;
-    EditText ed1, ed2;
+    EditText edi1, edi2;
 
-    TextView tx1;
+    TextView txt2;
     int counter = 3;
 
     @Override
@@ -35,27 +22,27 @@ public class Secundaria extends AppCompatActivity {
         setContentView(R.layout.activity_secundaria);
 
         btn_2 = (Button) findViewById(R.id.btn_2);
-        ed1 = (EditText) findViewById(R.id.ed1);
-        ed2 = (EditText) findViewById(R.id.edi2);
+        edi1 = (EditText) findViewById(R.id.edi1);
+        edi2 = (EditText) findViewById(R.id.edi2);
 
         btn_3 = (Button) findViewById(R.id.btn_3);
-        tx1 = (TextView) findViewById(R.id.tx1);
-        tx1.setVisibility(View.GONE);
+        txt2 = (TextView) findViewById(R.id.txt2);
+        txt2.setVisibility(View.GONE);
 
         btn_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ed1.getText().toString().equals("admin") &&
+                if (edi1.getText().toString().equals("admin") &&
 
-                        ed2.getText().toString().equals("admin")) {
-                    Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                        edi2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Redireccionar...", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Credenciales Incorrectas", Toast.LENGTH_SHORT).show();
 
-                    tx1.setVisibility(View.VISIBLE);
-                    tx1.setBackgroundColor(Color.RED);
+                    txt2.setVisibility(View.VISIBLE);
+                    txt2.setBackgroundColor(Color.RED);
                     counter--;
-                    tx1.setText(Integer.toString(counter));
+                    txt2.setText(Integer.toString(counter));
 
                     if (counter == 0) {
                         btn_2.setEnabled(false);
