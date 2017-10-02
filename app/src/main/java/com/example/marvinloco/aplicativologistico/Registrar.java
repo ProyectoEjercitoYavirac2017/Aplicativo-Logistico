733f09ac8c225d3a80f5b867e9222cb9a53c422b
Usuario <marquito.quito@hotmail.com>
2017-10-02 12:45:41 -0500

    cambios aplicativo 10:21

--- app/src/main/java/com/example/marvinloco/aplicativologistico/Registrar.java
+++ app/src/main/java/com/example/marvinloco/aplicativologistico/Registrar.java
@@ -1,13 +1,51 @@
 package com.example.marvinloco.aplicativologistico;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
 public class Registrar extends AppCompatActivity {
+    Button btn_2, btn_3;
+    EditText edi4, edi5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+
+        edi4 = (EditText) findViewById(R.id.edi4);
+        edi5 = (EditText) findViewById(R.id.edi5);
+        btn_2 = (Button) findViewById(R.id.btn_2);
+        btn_3 = (Button) findViewById(R.id.btn_3);
+
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edi4.getText().toString().equals("") &&
+
+                        edi5.getText().toString().equals("")) {
+                    Intent itemintent = new Intent(Registrar.this, Menu.class);
+                    Registrar.this.startActivity(itemintent);
+                    Toast.makeText(getApplicationContext(), "Redireccionar...", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Contraseñas Incorrectas", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btn_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
+
 }
+
+
--- app/src/main/java/com/example/marvinloco/aplicativologistico/Secundaria.java
+++ app/src/main/java/com/example/marvinloco/aplicativologistico/Secundaria.java
@@ -1,33 +1,26 @@
 package com.example.marvinloco.aplicativologistico;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.graphics.Color;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class Secundaria extends AppCompatActivity {
     Button btn_2, btn_3;
     EditText edi1, edi2;
 
-    TextView txt2;
-    int counter = 3;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secundaria);
 
-        btn_2 = (Button) findViewById(R.id.btn_2);
-        edi1 = (EditText) findViewById(R.id.edi1);
+        edi1 = (EditText) findViewById(R.id.txt1);
         edi2 = (EditText) findViewById(R.id.edi2);
-
+        btn_2 = (Button) findViewById(R.id.btn_2);
         btn_3 = (Button) findViewById(R.id.btn_3);
-        txt2 = (TextView) findViewById(R.id.txt2);
-        txt2.setVisibility(View.GONE);
 
         btn_2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,28 +28,24 @@ public class Secundaria extends AppCompatActivity {
                 if (edi1.getText().toString().equals("admin") &&
 
                         edi2.getText().toString().equals("admin")) {
+                    Intent itemintent = new Intent(Secundaria.this, Menu.class);
+                    Secundaria.this.startActivity(itemintent);
                     Toast.makeText(getApplicationContext(), "Redireccionar...", Toast.LENGTH_SHORT).show();
-                } else {
+                    } else {
                     Toast.makeText(getApplicationContext(), "Credenciales Incorrectas", Toast.LENGTH_SHORT).show();
-
-                    txt2.setVisibility(View.VISIBLE);
-                    txt2.setBackgroundColor(Color.RED);
-                    counter--;
-                    txt2.setText(Integer.toString(counter));
-
-                    if (counter == 0) {
-                        btn_2.setEnabled(false);
                     }
-                }
             }
         });
 
         btn_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
-            }
+                Intent itemintent = new Intent(Secundaria.this, Registrar.class);
+                Secundaria.this.startActivity(itemintent);
+                }
         });
     }
 
 }
+
+
